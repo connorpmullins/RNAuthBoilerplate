f6dc36e9d971c8a5b4d10fedfd6f4329bfa821a2
Connor Mullins <connorpmullins@gmail.com>
2021-10-19 10:48:45 -0700

    change linting rules, add custom path aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          assets: './assets',
+          components: './app/components',
+          navigators: './app/navigators',
+          screens: './app/screens',
+        },
+      },
+    ],
+  ],
 };

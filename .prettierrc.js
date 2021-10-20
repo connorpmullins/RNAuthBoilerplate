baa12ffb71c29493c58f667617d62efb6dbcaaf0
Connor Mullins <connorpmullins@gmail.com>
2021-10-20 03:14:50 -0700

    small changes, add github action

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
   bracketSpacing: false,
   jsxBracketSameLine: true,
   singleQuote: false,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
+  trailingComma: "all",
+  arrowParens: "avoid",
   semi: false,
 }

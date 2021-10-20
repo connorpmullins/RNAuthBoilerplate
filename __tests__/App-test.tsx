751af94a59952d3a95b1a0a353773604e09e62a7
Connor Mullins <connorpmullins@gmail.com>
2021-10-20 03:49:26 -0700

    get jest set up and working correctly

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -9,6 +9,8 @@ import App from "../app/App"
 // Note: test renderer must be required after react-native.
 import renderer from "react-test-renderer"
 
+jest.useFakeTimers()
+
 it("renders correctly", () => {
   renderer.create(<App />)
 })
